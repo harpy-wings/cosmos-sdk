9f4d3ca3eba1a1aa9cf45bf9750f6410e1cf950c
Julien Robert <julien@rbrt.fr>
2022-08-29 09:57:48 +0200

    chore: rename `github.com/gogo/protobuf` fork to `github.com/cosmos/gogoproto`

--- scripts/mockgen.sh
+++ scripts/mockgen.sh
@@ -7,7 +7,7 @@ $mockgen_cmd -source db/types.go -package mocks -destination tests/mocks/db/type
 $mockgen_cmd -source=types/module/module.go -package mocks -destination tests/mocks/types_module_module.go
 $mockgen_cmd -source=types/invariant.go -package mocks -destination tests/mocks/types_invariant.go
 $mockgen_cmd -source=types/router.go -package mocks -destination tests/mocks/types_router.go
-$mockgen_cmd -package mocks -destination tests/mocks/grpc_server.go github.com/gogo/protobuf/grpc Server
+$mockgen_cmd -package mocks -destination tests/mocks/grpc_server.go github.com/cosmos/gogoproto/grpc Server
 $mockgen_cmd -package mocks -destination tests/mocks/tendermint_tendermint_libs_log_DB.go github.com/tendermint/tendermint/libs/log Logger
 $mockgen_cmd -source=orm/model/ormtable/hooks.go -package ormmocks -destination orm/testing/ormmocks/hooks.go
 $mockgen_cmd -source=x/nft/expected_keepers.go -package testutil -destination x/nft/testutil/expected_keepers_mocks.go
